e37870fdefd476fcfa857d382d0b34d295a7da18
DonBatman <serfdon@gmail.com>
2015-05-07 17:05:53 -0700

    All concrete now has to dry

--- myconcrete/concrete.lua
+++ myconcrete/concrete.lua
@@ -11,28 +11,126 @@ local slope_cbox = {
 local slope_cbox_long = {
 	type = "fixed",
 	fixed = {
-		{-0.5, -0.5,   -1.5,  0.5, -0.375, 0.5},  --  NodeBox1
-		{-0.5, -0.375, -1.25, 0.5, -0.25,  0.5},  --  NodeBox2
-		{-0.5, -0.25,  -1,    0.5, -0.125, 0.5},  --  NodeBox3
-		{-0.5, -0.125, -0.75, 0.5,  0,     0.5},  --  NodeBox4
-		{-0.5,  0,     -0.5,  0.5,  0.125, 0.5},  --  NodeBox5
-		{-0.5,  0.125, -0.25, 0.5,  0.25,  0.5},  --  NodeBox6
-		{-0.5,  0.25,   0,    0.5,  0.375, 0.5},  --  NodeBox7
-		{-0.5,  0.375,  0.25, 0.5,  0.5,   0.5},  --  NodeBox8
+		{-0.5, -0.5,   -1.5,  0.5, -0.375, 0.5},
+		{-0.5, -0.375, -1.25, 0.5, -0.25,  0.5},
+		{-0.5, -0.25,  -1,    0.5, -0.125, 0.5},
+		{-0.5, -0.125, -0.75, 0.5,  0,     0.5}, 
+		{-0.5,  0,     -0.5,  0.5,  0.125, 0.5}, 
+		{-0.5,  0.125, -0.25, 0.5,  0.25,  0.5}, 
+		{-0.5,  0.25,   0,    0.5,  0.375, 0.5},  
+		{-0.5,  0.375,  0.25, 0.5,  0.5,   0.5},
 	}
 }
 
---Sidewalk
-minetest.register_node("myconcrete:sidewalk", {
-	description = "Sidewalk",
-	drawtype = "normal",
-	tiles = {"myconcrete_sidewalk.png"},
+local sep_wall_cbox = {
+	type = "fixed",
+	fixed = {
+		{-5/16, -1/2, -7/16, 5/16, -1/4, 7/16},
+		{-1/16, -1/4, -7/16, 1/16, 1/2, 7/16},
+		{-3/16, -1/2, -5/16, 3/16, 0, -1/4},
+		{-3/16, -1/2, 1/4, 3/16, 0, 5/16}
+	}
+}
+
+local cyl_cbox = {
+	type = "fixed",
+	fixed = {
+		{3/8, -1/2, -1/2, 1/2, 1/2, 1/2},
+		{-1/2, -1/2, -1/2, -3/8, 1/2, 1/2},
+		{-1/2, -1/2, 3/8, 1/2, 1/2, 1/2},
+		{-1/2, -1/2, -1/2, 1/2, 1/2, -3/8}
+	}
+}
+local fence_cbox = {
+	type = "fixed",
+	fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
+}
+
+local item_tab = { -- mat, descr, img, dtype, cbox
+{"sidewalk","Sidewalk","sidewalk","","normal",""},
+{"concrete","Concrete","concrete","","normal",""},
+
+{"ramp_sidewalk",	"Sidewalk Ramp",	"sidewalk_mesh",	"myconcrete_slope.obj","mesh",slope_cbox},
+{"ramp_sidewalk_long",	"Sidewalk Ramp Long",	"sidewalk_long_mesh",	"myconcrete_slope_long.obj","mesh",slope_cbox_long},
+
+{"ramp_concrete","Concrete Ramp","concrete_mesh","myconcrete_slope.obj","mesh",slope_cbox},
+{"ramp_concrete_long","Concrete Ramp Long","concrete_mesh","myconcrete_slope_long.obj","mesh",slope_cbox_long},
+
+{"precast_concrete_seperating_wall",	"Seperation Wall",	"concrete",	"","nodebox",sep_wall_cbox},
+{"precast_concrete_cylinder",	"Concrete Cylinder",	"concrete",	"","nodebox",cyl_cbox},
+{"fence_concrete",	"Concrete Fence",	"concrete",	"","fencelike",fence_cbox},
+}
+for i in ipairs (item_tab) do
+	local mat = item_tab[i][1]
+	local descr = item_tab[i][2]
+	local img = item_tab[i][3]
+	local obj = item_tab[i][4]
+	local dtype = item_tab[i][5]
+	local cbox = item_tab[i][6]
+
+local concrete_tab = { --num, desc, imgnum, gro
+{"","","",{cracky = 1}},
+{"3","3","^[colorize:black:100",{cracky = 1, not_in_creative_inventory = 1}},
+{"2","2","^[colorize:black:50",{cracky = 1, not_in_creative_inventory = 1}},
+{"1","1","^[colorize:black:25",{cracky = 1, not_in_creative_inventory = 1}},
+}
+for i in ipairs (concrete_tab) do
+	local num = concrete_tab[i][1]
+	local desc = concrete_tab[i][2]
+	local imgnum = concrete_tab[i][3]
+	local gro = concrete_tab[i][4]
+
+
+
+minetest.register_node("myconcrete:"..mat..num, {
+	description = descr..desc,
+	drawtype = dtype,
+	mesh = obj,
+	tiles = {"myconcrete_"..img..".png"..imgnum},
 	paramtype = "light",
 	paramtype2 = "facedir",
-	groups = {cracky=1},
-	stack_max = 250,
+	drop = "myconcrete:"..mat,
+	groups = gro,
+--	stack_max = 250,
 	sounds = default.node_sound_stone_defaults(),
+	node_box = cbox,
+	selection_box = cbox,
+	collision_box = cbox,
+
+after_place_node = function(pos, placer, itemstack, pointed_thing)
+	local node = minetest.get_node(pos)
+--	if node == "myconcrete:"..mat then
+	   minetest.set_node(pos, {name = "myconcrete:"..mat.."3", param2 = node.param2})
+--	end
+end,
 })
+
+
+minetest.register_abm({
+	nodenames = {"myconcrete:"..mat.."3"},
+	interval = 180.0,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		minetest.set_node(pos, {name = "myconcrete:"..mat.."2", param2 = node.param2})
+	end,
+})
+minetest.register_abm({
+	nodenames = {"myconcrete:"..mat.."2"},
+	interval = 180.0,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		minetest.set_node(pos, {name = "myconcrete:"..mat.."1", param2 = node.param2})
+	end,
+})
+minetest.register_abm({
+	nodenames = {"myconcrete:"..mat.."1"},
+	interval = 180.0,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		minetest.set_node(pos, {name = "myconcrete:"..mat, param2 = node.param2})
+	end,
+})
+
 --Craft
 minetest.register_craft({
 	output = "myconcrete:sidewalk 2",
@@ -42,79 +140,6 @@ minetest.register_craft({
 		{"", "",""},
 	}
 })
-
---Concrete
-minetest.register_node("myconcrete:concrete", {
-	description = "Concrete",
-	drawtype = "normal",
-	tiles = {"myconcrete_concrete4.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {cracky=1},
-	stack_max = 250,
-	sounds = default.node_sound_stone_defaults(),
-
-})
-minetest.register_node("myconcrete:concrete3", {
-	description = "Concrete",
-	drawtype = "normal",
-	tiles = {"myconcrete_concrete3.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drop = "myconcrete:concrete",
-	groups = {cracky=1, not_in_creative_inventory = 1},
-	stack_max = 250,
-	sounds = default.node_sound_stone_defaults(),
-
-})
-minetest.register_node("myconcrete:concrete2", {
-	description = "Concrete",
-	drawtype = "normal",
-	tiles = {"myconcrete_concrete2.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drop = "myconcrete:concrete",
-	groups = {cracky=1, not_in_creative_inventory = 1},
-	stack_max = 250,
-	sounds = default.node_sound_stone_defaults(),
-
-})
-minetest.register_node("myconcrete:concrete1", {
-	description = "Concrete",
-	drawtype = "normal",
-	tiles = {"myconcrete_concrete1.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drop = "myconcrete:concrete",
-	groups = {cracky=1, not_in_creative_inventory = 1},
-	stack_max = 250,
-	sounds = default.node_sound_stone_defaults(),
-
-})
-minetest.register_abm({
-	nodenames = {"myconcrete:concrete"},
-	interval = 180.0,
-	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		minetest.set_node(pos, {name = "myconcrete:concrete3"})
-	end,
-})
-minetest.register_abm({
-	nodenames = {"myconcrete:concrete3"},
-	interval = 180.0,
-	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		minetest.set_node(pos, {name = "myconcrete:concrete2"})
-	end,
-})
-minetest.register_abm({
-	nodenames = {"myconcrete:concrete2"},
-	interval = 180.0,
-	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		minetest.set_node(pos, {name = "myconcrete:concrete1"})
-	end,
-})
 --Craft
 minetest.register_craft({
 	type = "shapeless",
@@ -122,20 +147,6 @@ minetest.register_craft({
 	recipe ={"myconcrete:lime", "default:gravel"},
 })
 
--- Concrete fence
-	minetest.register_node("myconcrete:fence_concrete", {
-		description = "Concrete fence",
-		drawtype = "fencelike",
-		tiles = {"myconcrete_concrete.png"},
-		paramtype = "light",
-		is_ground_content = true,
-		selection_box = {
-			type = "fixed",
-			fixed = {-1/8, -1/2, -1/8, 1/8, 1/2, 1/8},
-		},
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-})
 --Craft
 minetest.register_craft({
 	output = "myconcrete:fence_concrete 6",
@@ -145,34 +156,6 @@ minetest.register_craft({
 	}
 })
 
--- Concrete seperating wall
-	minetest.register_node("myconcrete:precast_concrete_seperating_wall", {
-		description = "Precast concrete seperating wall",
-		tile_images = {"myconcrete_concrete.png"},
-		drawtype = "nodebox",
-		paramtype = "light",
-		paramtype2 = "facedir",
-		groups = {cracky = 2},
-		sounds = default.node_sound_stone_defaults(),
-		node_box = {
-			type = "fixed",
-			fixed = {
-				{-5/16, -1/2, -7/16, 5/16, -1/4, 7/16},
-				{-1/16, -1/4, -7/16, 1/16, 1/2, 7/16},
-				{-3/16, -1/2, -5/16, 3/16, 0, -1/4},
-				{-3/16, -1/2, 1/4, 3/16, 0, 5/16}
-			}
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {
-				{-5/16, -1/2, -7/16, 5/16, -1/4, 7/16},
-				{-1/16, -1/4, -7/16, 1/16, 1/2, 7/16},
-				{-3/16, -1/2, -5/16, 3/16, 0, -1/4},
-				{-3/16, -1/2, 1/4, 3/16, 0, 5/16}
-			}
-		}
-	})
 --Craft
 minetest.register_craft({
 	output = "myconcrete:precast_concrete_seperating_wall 5",
@@ -182,35 +165,7 @@ minetest.register_craft({
 		{'myconcrete:concrete', 'myconcrete:concrete', 'myconcrete:concrete'}
 	}
 })
--- Concrete cylinder
-	minetest.register_node("myconcrete:precast_concrete_cylinder", {
-		description = "Precast concrete cylinder",
-		tile_images = {"myconcrete_concrete.png"},
-		drawtype = "nodebox",
-		paramtype = "light",
-		paramtype2 = "facedir",
-		groups = {cracky = 2},
-		walkable = false,
-		climbable = true,
-		node_box = {
-			type = "fixed",
-			fixed = {
-				{3/8, -1/2, -1/2, 1/2, 1/2, 1/2},
-				{-1/2, -1/2, -1/2, -3/8, 1/2, 1/2},
-				{-1/2, -1/2, 3/8, 1/2, 1/2, 1/2},
-				{-1/2, -1/2, -1/2, 1/2, 1/2, -3/8}
-			}
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {
-				{3/8, -1/2, -1/2, 1/2, 1/2, 1/2},
-				{-1/2, -1/2, -1/2, -3/8, 1/2, 1/2},
-				{-1/2, -1/2, 3/8, 1/2, 1/2, 1/2},
-				{-1/2, -1/2, -1/2, 1/2, 1/2, -3/8}
-			}
-		}
-	})
+
 --Craft
 minetest.register_craft({
 	output = "myconcrete:precast_concrete_cylinder 8",
@@ -220,20 +175,7 @@ minetest.register_craft({
 		{'myconcrete:concrete', 'myconcrete:concrete', 'myconcrete:concrete'}
 	}
 })
---Sidewalk Ramp
-minetest.register_node("myconcrete:ramp_sidewalk", {
-	description = "Sidewalk Ramp",
-	drawtype = "mesh",
-	mesh = "myconcrete_slope.obj",
-	tiles = {"myconcrete_sidewalk_mesh.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {cracky = 1},
-	sounds = default.node_sound_wood_defaults(),
-	on_place = minetest.rotate_node,
-	collision_box = slope_cbox,
-	selection_box = slope_cbox
-})
+
 --Craft
 minetest.register_craft({
 	output = "myconcrete:ramp_sidewalk 2",
@@ -242,19 +184,6 @@ minetest.register_craft({
 		{"", "","myconcrete:sidewalk"},
 		{"", "myconcrete:sidewalk","myconcrete:sidewalk"},
 	}
-})
-minetest.register_node("myconcrete:ramp_sidewalk_long", {
-	description = "Sidewalk Ramp Long",
-	drawtype = "mesh",
-	mesh = "myconcrete_slope_long.obj",
-	tiles = {"myconcrete_sidewalk_long_mesh.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {cracky = 1},
-	sounds = default.node_sound_wood_defaults(),
-	on_place = minetest.rotate_node,
-	collision_box = slope_cbox_long,
-	selection_box = slope_cbox_long
 })
 --Craft
 minetest.register_craft({
@@ -266,20 +195,6 @@ minetest.register_craft({
 	}
 })
 
---Concrete Ramp
-minetest.register_node("myconcrete:ramp_concrete", {
-	description = "Concrete Ramp",
-	drawtype = "mesh",
-	mesh = "myconcrete_slope.obj",
-	tiles = {"myconcrete_concrete_mesh.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {cracky = 1},
-	sounds = default.node_sound_wood_defaults(),
-	on_place = minetest.rotate_node,
-	collision_box = slope_cbox,
-	selection_box = slope_cbox
-})
 --Craft
 minetest.register_craft({
 	output = "myconcrete:ramp_concrete 2",
@@ -289,19 +204,8 @@ minetest.register_craft({
 		{"", "myconcrete:concrete","myconcrete:concrete"},
 	}
 })
-minetest.register_node("myconcrete:ramp_concrete_long", {
-	description = "concrete Ramp Long",
-	drawtype = "mesh",
-	mesh = "myconcrete_slope_long.obj",
-	tiles = {"myconcrete_concrete_mesh.png"},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	groups = {cracky = 1},
-	sounds = default.node_sound_wood_defaults(),
-	on_place = minetest.rotate_node,
-	collision_box = slope_cbox_long,
-	selection_box = slope_cbox_long
-})
+
+
 --Craft
 minetest.register_craft({
 	output = "myconcrete:ramp_concrete_long 2",
@@ -312,3 +216,5 @@ minetest.register_craft({
 	}
 })
 
+end
+end
