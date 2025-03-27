2e04c7674a06fd08e73242afae0738ac94027e88
Don <serfdon@gmail.com>
2025-03-27 10:52:29 -0700

    Add files via upload

--- mycobble/init.lua
+++ mycobble/init.lua
@@ -3,24 +3,28 @@ minetest.register_craft({
 	output = "default:gravel",
 	recipe ={"default:cobble"},
 })
+
 minetest.register_craft({
 	type = "shapeless",
 	output = "default:sand",
 	recipe ={"default:gravel"},
 })
+
 minetest.register_craft({
 	type = "shapeless",
-	output = "default:desert_gravel",
+	output = "mycobble:desert_gravel",
 	recipe ={"default:desert_cobble"},
 })
+
 minetest.register_craft({
 	type = "shapeless",
 	output = "default:desert_sand",
-	recipe ={"default:desert_stone"},
+	recipe ={"mycobble:desert_gravel"},
 })
+
 minetest.register_node ("mycobble:desert_gravel",{
 	description = "Desert Gravel",
-	tiles = {"mycobble_desert_gravel.png"},
+	tiles = {"default_gravel.png^[colorize:#8A2908:120"},
 	is_ground_content = true,
 	groups = {crumbly=2, falling_node=1},
 	sounds = default.node_sound_dirt_defaults({
